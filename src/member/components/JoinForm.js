0c1089d7a8aa125f90b47fb6598b51e66a221ef2
yeonhwa001 <chyh202@naver.com>
2024-04-16 17:53:04 +0900

    no message

--- src/member/components/JoinForm.js
+++ src/member/components/JoinForm.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import {styled} from 'styled-components';
-import { FaCheckSquare,FaRegCheckSquare } from "react-icons/fa";
+import { styled } from 'styled-components';
+import { FaCheckSquare, FaRegCheckSquare } from 'react-icons/fa';
 import { BigButton, ButtonGroup } from '../../commons/components/Buttons';
 import InputBox from '../../commons/components/InputBox';
 
-const FormBox = styled.form`
-
-`;
+const FormBox = styled.form``;
 
 const JoinForm = () => {
   const { t } = useTranslation();
@@ -16,13 +14,13 @@ const JoinForm = () => {
       <dl>
         <dt>{t('이메일')}</dt>
         <dd>
-          <InputBox type='text' />
+          <InputBox type="text" />
         </dd>
       </dl>
       <dl>
         <dt>{t('비밀번호')}</dt>
         <dd>
-          <InputBox type='password' />
+          <InputBox type="password" />
         </dd>
       </dl>
       <dl>
@@ -34,22 +32,22 @@ const JoinForm = () => {
       <dl>
         <dt>{t('회원명')}</dt>
         <dd>
-          <InputBox type='text' />
+          <InputBox type="text" />
         </dd>
       </dl>
 
-      <div className='terms-agree'>
-        <FaRegCheckSquare />{t('회원가입_약관에_동의합니다.')}
-
+      <div className="terms-agree">
+        <FaRegCheckSquare />
+        {t('회원가입_약관에_동의합니다.')}
       </div>
 
       <ButtonGroup width={450}>
-      <BigButton type="reset" color="light">
-        {t('다시입력')}
-      </BigButton>
-      <BigButton type="submit" color="dark">
-        {t('가입하기')}
-      </BigButton>
+        <BigButton type="reset" color="light">
+          {t('다시입력')}
+        </BigButton>
+        <BigButton type="submit" color="dark">
+          {t('가입하기')}
+        </BigButton>
       </ButtonGroup>
     </FormBox>
   );
